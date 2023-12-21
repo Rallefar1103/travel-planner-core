797c3faa8096d7f69a40ec16a89bc82129e2be57
Rallefar1103 <rasmus@turftank.com>
2023-12-21 00:45:04 -0800

    updates the mongoDB schema for itinerary

--- src/models/itinerary.js
+++ src/models/itinerary.js
@@ -1,9 +1,27 @@
 const mongoose = require("mongoose");
 
+const diningOptionsSchema = new mongoose.Schema({
+  type: String,
+  cuisine: String,
+  priceRange: String,
+});
+
+const attractionOptionsSchema = new mongoose.Schema({
+  type: [String], // Array of strings
+});
+
+const userPreferencesSchema = new mongoose.Schema({
+  diningOptions: diningOptionsSchema,
+  attractionOptions: attractionOptionsSchema,
+});
+
 const itinerarySchema = new mongoose.Schema({
   title: String,
-  description: String,
-  recommendations: String,
+  destination: String,
+  duration: String,
+  budget: String,
+  userPreferences: userPreferencesSchema,
+  recommendedItinerary: String, // or another Schema if it's more complex
 });
 
 module.exports = mongoose.model("Itinerary", itinerarySchema);
