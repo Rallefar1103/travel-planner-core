8cb740cdbae6ee0df4341944af0aa1348f5108f9
Rallefar1103 <rasmus@turftank.com>
2023-12-20 19:11:38 -0800

    adds depenendecies

--- src/routes/itineraryRoutes.js
+++ src/routes/itineraryRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 const Itinerary = require("../models/itinerary");
 
 const recommenderURL = process.env.RECOMMENDER_URL;
